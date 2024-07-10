77e4c1c90cc469e5ff6f7e1b1dd85023c8f02657
Nomkhosi Ngomane <khosipn210@gmail.com>
2024-07-10 21:29:21 +0200

    Added functionality for the select

--- src/script.js
+++ src/script.js
@@ -1,27 +1,62 @@
-setInterval(function () {
+function updateTime() {
   let JoziElement = document.querySelector("#Jozi");
-  let JoziDateElement = JoziElement.querySelector(".date");
-  let JoziTimeElement = JoziElement.querySelector(".time");
-  let JoziTime = moment().tz("Africa/Johannesburg");
+  if (JoziElement) {
+    let JoziDateElement = JoziElement.querySelector(".date");
+    let JoziTimeElement = JoziElement.querySelector(".time");
+    let JoziTime = moment().tz("Africa/Johannesburg");
 
-  JoziDateElement.innerHTML = JoziTime.format("MMMM Do YYYY");
-  JoziTimeElement.innerHTML = JoziTime.format("h:mm:ss [<small>]A[</small>]");
+    JoziDateElement.innerHTML = JoziTime.format("MMMM Do YYYY");
+    JoziTimeElement.innerHTML = JoziTime.format("h:mm:ss [<small>]A[</small>]");
+  }
 
   let NairobiElement = document.querySelector("#Nairobi");
-  let NairobiDateElement = NairobiElement.querySelector(".date");
-  let NairobiTimeElement = NairobiElement.querySelector(".time");
-  let NairobiTime = moment().tz("Africa/Nairobi");
+  if (NairobiElement) {
+    let NairobiDateElement = NairobiElement.querySelector(".date");
+    let NairobiTimeElement = NairobiElement.querySelector(".time");
+    let NairobiTime = moment().tz("Africa/Nairobi");
 
-  NairobiDateElement.innerHTML = NairobiTime.format("MMMM Do YYYY");
-  NairobiTimeElement.innerHTML = NairobiTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    NairobiDateElement.innerHTML = NairobiTime.format("MMMM Do YYYY");
+    NairobiTimeElement.innerHTML = NairobiTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   let AccraElement = document.querySelector("#Accra");
-  let AccraDateElement = AccraElement.querySelector(".date");
-  let AccraTimeElement = AccraElement.querySelector(".time");
-  let AccraTime = moment().tz("Africa/Accra");
+  if (AccraElement) {
+    let AccraDateElement = AccraElement.querySelector(".date");
+    let AccraTimeElement = AccraElement.querySelector(".time");
+    let AccraTime = moment().tz("Africa/Accra");
 
-  AccraDateElement.innerHTML = AccraTime.format("MMMM Do YYYY");
-  AccraTimeElement.innerHTML = AccraTime.format("h:mm:ss [<small>]A[</small>]");
-}, 1000);
+    AccraDateElement.innerHTML = AccraTime.format("MMMM Do YYYY");
+    AccraTimeElement.innerHTML = AccraTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  letcityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+    <div class="city">
+                <div>
+                    <h2>${cityTimeZone}</h2>
+                    <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+                </div>
+                <div class="time">${cityTime.format(
+                  "h:mm:ss"
+                )}<small>${cityTime.format("A")}</div>
+            </div>
+  `;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+
+citiesSelectElement.addEventListener("change", updateCity);
