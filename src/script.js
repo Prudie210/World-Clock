299b01ff4611a0320271d48d5d53f45509d226e1
Nomkhosi Ngomane <khosipn210@gmail.com>
2024-07-11 19:44:08 +0200

    Add link to all cities

--- src/script.js
+++ src/script.js
@@ -52,6 +52,7 @@ function updateCity(event) {
                   "h:mm:ss"
                 )} <small>${cityTime.format("A")}</div>
             </div>
+              <a href="/">All cities</a>
   `;
 }
 
